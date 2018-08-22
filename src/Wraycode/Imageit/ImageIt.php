86962c96ef1116dba36f29f2d58247ca6d5ea995
wraycode <wraycode@gmail.com>
2018-08-22 13:55:59 -0600

    Create ImageIt.php

---
+++ src/Wraycode/Imageit/ImageIt.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Wraycode\ImageIt;
+
+/**
+ * A terribly useful class
+ *
+ * See: http://knpuniversity.com/screencast/question-answer-day
+ */
+class ImageIt
+{
+    public function getDescription()
+    {
+        return 'Image Manipulation Package';
+    }
+}
